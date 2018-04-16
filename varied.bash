cc73684e83106c26146086d848cee6493e461278
Matteo Di Giorgio <m.digiorgio@wedoit.io>
2018-04-16 14:12:18 +0200

    new command
    new command

--- varied.bash
+++ varied.bash
@@ -1,10 +1,13 @@
 find . -name discount_input.csv* -print -exec tar -cvf /tmp/discount_to_transfer.tar {} '+'
 
-compress file
+# compress file
 tar -czvf name-of-archive.tar.gz /path/to/directory-or-file
 
-read content of tar gz file
+# read content of tar gz file
 tar tvf GENSTAT.tar.gz
 
-order file recurse by date
+# order file recurse by date
 find . -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort | cut -f 2- -d ' '
+
+# find file date
+find . -type f -ls |grep 'Apr 16 10:'
