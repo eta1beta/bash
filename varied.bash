bd457e918e45563cca7236e0cee867d3a96b3995
Matteo Di Giorgio <m.digiorgio@wedoit.io>
2018-04-16 14:11:09 +0200

    new
    new

--- varied.bash
+++ varied.bash
@@ -1,4 +1,10 @@
 find . -name discount_input.csv* -print -exec tar -cvf /tmp/discount_to_transfer.tar {} '+'
 
-
+compress file
 tar -czvf name-of-archive.tar.gz /path/to/directory-or-file
+
+read content of tar gz file
+tar tvf GENSTAT.tar.gz
+
+order file recurse by date
+find . -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort | cut -f 2- -d ' '
