11cc24834c03e2c7318a9f255d804a9a5c341a26
Matteo Di Giorgio <m.digiorgio@wedoit.io>
2018-04-20 12:07:39 +0200

    add
    add

--- varied.sh
+++ varied.sh
@@ -10,15 +10,6 @@
 # find pipe tar
 find . -name discount_input.csv* -print -exec tar -cvf /tmp/discount_to_transfer.tar {} '+'
 
-# compress file
-tar -czvf name-of-archive.tar.gz /path/to/directory-or-file
-
-# read content of tar gz file
-tar tvf GENSTAT.tar.gz
-
-# EXTRACT CONTENT
-tar xvf GENSTAT.tar.gz
-
 # order file recurse by date
 find . -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort | cut -f 2- -d ' '
 
@@ -27,6 +18,15 @@ find . -type f -ls |grep 'Apr 16 10:'
 
 #find file date without sh
 find . ! -name '*.sh'  -type f -ls |grep 'Apr 16'|xargs ls -ltr
+
+# compress file
+tar -czvf name-of-archive.tar.gz /path/to/directory-or-file
+
+# read content of tar gz file
+tar tvf GENSTAT.tar.gz
+
+# EXTRACT CONTENT
+tar xvf GENSTAT.tar.gz
 
 # test lftp in WEDO
 lftp -u teclatest,testtecla -p 226  -e "put ./sunglasshutBV.xml;quit" sftp://sftp.wedoit.io
@@ -42,6 +42,13 @@ fi
 #SEND MAIL WITH MAILX
 echo "mail text." | mailx -s "object text ${targetEnv}" -a "file to send" "${EMAIL_STATISTICS} a@b.com b@c.com" 
 
+# usage: ./emailer.sh $1 $2 $3 $4
+EMAIL_OBJECT=$1 	# Ex: "Please find the AU Sales Report executed `date` attached to this email."
+EMAIL_SUBJECT=$2 	# Ex: "AU Store Associate Sales Report"
+EMAIL_ATTACH=$3 	# Ex: "./tmp/appo/file.zip"
+EMAIL_TO=$4 		# Ex: "n.surname@b.com b.surname@c.com"
+echo ${EMAIL_OBJECT}  | mailx -s ${EMAIL_SUBJECT} -a ${EMAIL_ATTACH} ${EMAIL_TO} 
+
 ## java command
 ${JAVA} -cp ./GenStatsOutbound.jar:/opt/ibm/db2/V10.5/java/db2java.zip:/opt/ibm/db2/V10.5/java/db2jcc.jar:/opt/ibm/db2/V10.5/java/db2jcc_license_cu.jar:. ${GENSTATS_ATTR} ${DBUSER1} ${DBPWD1} ${DBURLPARM} 
 
@@ -51,3 +58,4 @@ zip -j /tmp/AU_Genstats_`date "+%m%d%y"`.zip /home/wasadmin/dataload/sgh/Outboun
 
 ## delete something inside zip
 zip -d /tmp/AU_Genstats_`date "+%m%d%y"`.zip WCSDC*
+
