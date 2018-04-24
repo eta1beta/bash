3587893133b3e76c9e0e6b477ff87f2b7e610133
Matteo Di Giorgio <m.digiorgio@wedoit.io>
2018-04-24 13:00:27 +0200

    added comment

--- varied.sh
+++ varied.sh
@@ -7,7 +7,7 @@
 ####################################################################
 
 
-# find pipe tar
+# find pipe tar 
 find . -name discount_input.csv* -print -exec tar -cvf /tmp/discount_to_transfer.tar {} '+'
 
 # order file recurse by date
@@ -34,23 +34,23 @@ lftp -u teclatest,testtecla -p 226  -e "put ./sunglasshutBV.xml;quit" sftp://sft
 # TEST exist file
 if [ -e "${GENSTATS_DIR}/${targetEnv}/sghStatistics/sghStatistics_Files.zip" ]; then
     echo "File exists"
-    
-else 
+
+else
 	echo "File doesn't exist"
 fi
 
 #SEND MAIL WITH MAILX
-echo "mail text." | mailx -s "object text ${targetEnv}" -a "file to send" "${EMAIL_STATISTICS} a@b.com b@c.com" 
+echo "mail text." | mailx -s "object text ${targetEnv}" -a "file to send" "${EMAIL_STATISTICS} a@b.com b@c.com"
 
 # usage: ./emailer.sh $1 $2 $3 $4
 EMAIL_OBJECT=$1 	# Ex: "Please find the AU Sales Report executed `date` attached to this email."
 EMAIL_SUBJECT=$2 	# Ex: "AU Store Associate Sales Report"
 EMAIL_ATTACH=$3 	# Ex: "./tmp/appo/file.zip"
 EMAIL_TO=$4 		# Ex: "n.surname@b.com b.surname@c.com"
-echo ${EMAIL_OBJECT}  | mailx -s ${EMAIL_SUBJECT} -a ${EMAIL_ATTACH} ${EMAIL_TO} 
+echo ${EMAIL_OBJECT}  | mailx -s ${EMAIL_SUBJECT} -a ${EMAIL_ATTACH} ${EMAIL_TO}
 
 ## java command
-${JAVA} -cp ./GenStatsOutbound.jar:/opt/ibm/db2/V10.5/java/db2java.zip:/opt/ibm/db2/V10.5/java/db2jcc.jar:/opt/ibm/db2/V10.5/java/db2jcc_license_cu.jar:. ${GENSTATS_ATTR} ${DBUSER1} ${DBPWD1} ${DBURLPARM} 
+${JAVA} -cp ./GenStatsOutbound.jar:/opt/ibm/db2/V10.5/java/db2java.zip:/opt/ibm/db2/V10.5/java/db2jcc.jar:/opt/ibm/db2/V10.5/java/db2jcc_license_cu.jar:. ${GENSTATS_ATTR} ${DBUSER1} ${DBPWD1} ${DBURLPARM}
 
 
 ## compress thing with zip
@@ -58,4 +58,3 @@ zip -j /tmp/AU_Genstats_`date "+%m%d%y"`.zip /home/wasadmin/dataload/sgh/Outboun
 
 ## delete something inside zip
 zip -d /tmp/AU_Genstats_`date "+%m%d%y"`.zip WCSDC*
-
