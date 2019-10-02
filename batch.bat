ee3c0a5feaafe19b16f904cd7cacad9fd7fb926e
Matteo Di Giorgio <m.digiorgio@wedoit.io>
2019-10-02 11:34:11 +0200

    Done

--- batch.bat
+++ batch.bat
@@ -1,4 +1,4 @@
-cd 
+cd
 
 FORFILES /S /D -10 /C "cmd /c IF @isdir == TRUE rd /S /Q @path"
 
@@ -11,7 +11,7 @@ for /R /D %s in (.\*) do @echo %s
 ## ----------------
 
 
-for /R /D %s in (.\*) do 
+for /R /D %s in (.\*) do
 
 cd $s
 FORFILES /S /D -10 /C "cmd /c IF @isdir == TRUE rd /S /Q $s
@@ -19,13 +19,13 @@ FORFILES /S /D -10 /C "cmd /c IF @isdir == TRUE rd /S /Q $s
 FORFILES /S /D -15 /C "cmd /c IF @isdir == TRUE rd /S /Q @path"
 
 ---------------------------
-REM DEL OLDEST DIR IN DIR 
+REM DEL OLDEST DIR IN DIR
 cd \uranium\aruba
 FOR /f "delims=" %%a in ('dir ".\*" /t:c /a:d /o:-d /b') do set lastfolder=%%a
 echo %lastfolder%
 rd /s /q ".\%lastfolder%"
 -----------------------------
 
- 
- 
+   
+
 http://www.m-link.it/m-link_news/Script_batch_bat_per_eliminare_cancellare_file_piu_vecchi_di_un_certo_numero_di_giorni.asp
